0a0a3f053677a1feed1ddf73ce5f707b7d74f81e
purisquiri <mau.dorigo@outlook.es>
2020-11-15 18:10:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+document.body.appendChild(newHeader);
+
+newHeader.innerHTML = "Mauro is the champion";
